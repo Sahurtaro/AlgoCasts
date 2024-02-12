7d6e3621d41cad03ef318a3b0b23ffad4c122aa0
santiago hurtado <sahurtaro@gmail.com>
2024-02-12 15:44:58 -0500

    21. Reversing an int solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//   if (n === 0) {
+//     return 0;
+//   } else if (n > 0) {
+//     const reversed = n.toString().split('').reverse().join('');
+//     return parseInt(reversed);
+//   } else if (n < 0) {
+//     let reverso = n.toString().split('').reverse().join('');
+//     const negative = parseInt(reverso);
+
+//     return negative * -1;
+//   }
+// }
+
+function reverseInt(n) {
+  const reversed = n.toString().split('').reverse().join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+//separar, revertir los caracteres y pegarlos de vuelta
